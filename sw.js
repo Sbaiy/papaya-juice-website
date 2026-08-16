df0bc107e59ea7ec6abb221030a40456cacb9a6e
Sbaiy <111650754+Sbaiy@users.noreply.github.com>
2026-08-16 12:57:44 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,9 +8,9 @@
    ✓ Notifications push
 ═══════════════════════════════════════════════════════ */
 
-const CACHE_STATIC  = 'papaya-static-v15';
-const CACHE_IMAGES  = 'papaya-images-v15';
-const CACHE_FONTS   = 'papaya-fonts-v15';
+const CACHE_STATIC  = 'papaya-static-v16';
+const CACHE_IMAGES  = 'papaya-images-v16';
+const CACHE_FONTS   = 'papaya-fonts-v16';
 
 const STATIC_ASSETS = [
   '/logo.png',
